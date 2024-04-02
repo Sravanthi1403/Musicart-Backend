cfd534c262cccce338ca98055e1d3b148415fc14
A Sravanthi <120970731+Sravanthi1403@users.noreply.github.com>
2024-04-02 10:48:44 +0530

    Update cookie.js

--- utils/cookie.js
+++ utils/cookie.js
@@ -5,8 +5,7 @@ const setCookie = (res, user_id, statusCode, message) => {
 
     res.status(statusCode).cookie('token' , token , { 
         httpOnly : true, 
-        maxAge : 24 * 60 * 60 * 1000,
-        sameSite : "none", 
+        maxAge : 24 * 60 * 60 * 1000, 
         secure :  true
     }).json({
         success : true,
