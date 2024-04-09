4f587b9573e3340ed399b5c2244a008f9bfa0eb9
A Sravanthi <120970731+Sravanthi1403@users.noreply.github.com>
2024-04-09 17:25:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 dotenv.config({path: './.env'});
 app.use(cookieParser());
 app.use(cors({
-    origin : 'musicart-frontend2-1pa020xsu-sravanthis-projects-bdbcceab.vercel.app',
+    origin : 'https://musicart-frontend2-1pa020xsu-sravanthis-projects-bdbcceab.vercel.app',
     methods : [ "GET","POST","PUT","PATCH", "DELETE" ],
     credentials : true,
 }));
