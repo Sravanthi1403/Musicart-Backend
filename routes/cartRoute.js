1e09ce73fd34f4a0d8155d1a1f05344888c516e3
A Sravanthi <120970731+Sravanthi1403@users.noreply.github.com>
2024-04-09 21:47:28 +0530

    Update cartRoute.js

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const cartControllers = require('../controllers/cartController');
+const isAuthenticated = require('../middlewares/authMiddleware');
 
-router.route('/addProductToCart').post(cartControllers.addProductToCart);
-router.route('/getCartProducts').get(cartControllers.getCartProducts);
-router.route('/updateQuantity').put(cartControllers.updateCartItemQuantity);
-router.route('/deleteAllItems').delete(cartControllers.deleteAllItems);
+
+router.route('/addProductToCart').post(isAuthenticated, cartControllers.addProductToCart);
+router.route('/getCartProducts').get(isAuthenticated, cartControllers.getCartProducts);
+router.route('/updateQuantity').put(isAuthenticated, cartControllers.updateCartItemQuantity);
+router.route('/deleteAllItems').delete(isAuthenticated, cartControllers.deleteAllItems);
 
 
 module.exports = router;
