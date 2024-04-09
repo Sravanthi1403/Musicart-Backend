9c5c5d45672f01a280e1b3ea341175b5a71ca2f2
A Sravanthi <120970731+Sravanthi1403@users.noreply.github.com>
2024-04-09 16:27:56 +0530

    Update cartModel.js

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
     productId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Product",
